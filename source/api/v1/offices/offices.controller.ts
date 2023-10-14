d8a0f9d90b62c1675b0f10d9a146fe1c9f214729
winter4 <mywinter4@yandex.ru>
2023-10-15 01:19:29 +0300

    feat: office fetch

--- source/api/v1/offices/offices.controller.ts
+++ source/api/v1/offices/offices.controller.ts
@@ -1,6 +1,7 @@
 import Database from "@src/clients/database";
 import {Request, Response} from "express";
 import {uuid} from "@src/types";
+import {Prisma} from "@prisma/client";
 
 const db = Database.instance;
 
@@ -16,19 +17,33 @@ export default {
 
     const offices = await db.$queryRaw`--sql
       SELECT 
-        o.* 
+        o.*,
+        COUNT("q"."id") as queuesCount
       FROM 
         "Office" as o
         LEFT JOIN "OfficeService" as os ON "os"."officeId" = "o"."id"
         LEFT JOIN "OfficeInclusion" as oi ON "oi"."officeId" = "o"."id"
+        LEFT JOIN "Queue" as q ON "q"."officeId" = "o"."id" AND "q"."serviceId" = ${serviceId}
       WHERE 
         "os"."serviceId" = ${serviceId}
+        --AND "q"."status" = 'PENDING' AND "q"."owner" = 'OTHER'
       GROUP BY 
         "o"."id", "oi"."inclusionId"
-      HAVING
-        "oi"."inclusionId" = any(${inclusionIds});
-    `;
+        --, "q"."serviceId", "q"."ticket", "q"."status", "q"."finishedAt", "q"."createdAt", "q"."officeId", "q"."owner", "q"."id"
+       ${
+         inclusionIds.length
+           ? Prisma.sql`HAVING "oi"."inclusionId" = any(${inclusionIds})`
+           : Prisma.empty
+       }
+       ORDER BY "queuescount" ASC
+    ;`;
 
-    res.json({data: offices});
+    res.json(
+      JSON.parse(
+        JSON.stringify({data: offices}, (_, v) =>
+          typeof v === "bigint" ? v.toString() : v
+        )
+      )
+    );
   },
 };
