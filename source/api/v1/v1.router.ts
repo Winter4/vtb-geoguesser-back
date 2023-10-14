74cd0644f611c99a6ac2d9511d71bb4c06419ae8
winter4 <mywinter4@yandex.ru>
2023-10-14 05:42:06 +0300

    feat: enable router

--- source/api/v1/v1.router.ts
+++ source/api/v1/v1.router.ts
@@ -1,5 +1,13 @@
 import { Router } from "express";
 
+import spots from "./spots/spots.router";
+import inclusions from "./inclusions/inclusions.router";
+import services from "./services/services.router";
+
 const v1 = Router();
+
+v1.use("/spots", spots);
+v1.use("/inclusions", inclusions);
+v1.use("/services", services);
 
 export default v1;
