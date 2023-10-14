a37a8c40a8760482d6081107998682a6b90dc00a
winter4 <mywinter4@yandex.ru>
2023-10-14 13:59:05 +0300

    feat: add union to spots#get

--- source/api/v1/spots/spots.controller.ts
+++ source/api/v1/spots/spots.controller.ts
@@ -19,7 +19,26 @@ export default {
       LEFT JOIN "OfficeInclusion" as oi ON "oi"."officeId" = "o"."id"
       LEFT JOIN "Inclusion" as i ON "i"."id" = "oi"."inclusionId"
 
-      GROUP BY "o"."id", "os"."serviceId", "s"."name", "oi"."inclusionId", "i"."name";
+      GROUP BY "o"."id", "os"."serviceId", "s"."name", "oi"."inclusionId", "i"."name"
+      
+      --
+      UNION
+      --
+
+      SELECT 
+        atm.*,
+        jsonb_agg(jsonb_build_object('id', "serviceId", 'name', "s"."name")) as services, 
+        jsonb_agg(jsonb_build_object('id', "inclusionId", 'name', "i"."name")) as inclusions 
+
+      FROM "ATM" as atm
+
+      LEFT JOIN "WorkTime" as wt ON "wt"."atmId" = "atm"."id"
+      LEFT JOIN "ATMService" as atms ON "atms"."atmId" = "atm"."id"
+      LEFT JOIN "Service" as s ON "s"."id" = "atms"."serviceId"
+      LEFT JOIN "ATMInclusion" as atmi ON "atmi"."atmId" = "atm"."id"
+      LEFT JOIN "Inclusion" as i ON "i"."id" = "atmi"."inclusionId"
+
+      GROUP BY "atm"."id", "atms"."serviceId", "s"."name", "atmi"."inclusionId", "i"."name";
     `;
 
     res.json({ data: result });
